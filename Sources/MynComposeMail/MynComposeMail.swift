c74b58915a004d7ecb0176c9a14c78fcf2c1cf36
ArunPIos <arunpandian2041@gmail.com>
2024-03-15 16:14:07 +0530

    Source File

---
+++ Sources/MynComposeMail/MynComposeMail.swift
@@ -0,0 +1,11 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import Foundation
+
+public struct MynMailSDK{
+    public static func setConfig(){
+        print("config")
+    }
+    
+}
